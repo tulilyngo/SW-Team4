7687c3642999d5f8cc8846c002cf4be6d86551f6
tulilyngo <tulilyngo@gmail.com>
2022-04-25 22:54:33 -0500

    reformat create account related classes; fix error handling

--- src/Database/CreateAccountData.java
+++ src/Database/CreateAccountData.java
@@ -2,46 +2,44 @@ package Database;
 
 import java.io.Serializable;
 
-public class CreateAccountData implements Serializable
-{
-  private String username;
-  private String password;
-  private String password2nd;
+public class CreateAccountData implements Serializable {
+    private String username;
+    private String password;
+    private String password2nd;
 
-  public String getUsername()
-  {
-    return username;
-  }
+    public String getUsername()
+    {
+        return username;
+    }
 
-  public String getPassword()
-  {
-    return password;
-  }
-  
-  public String getPassword2nd()
-  {
-    return password2nd;
-  }
+    public String getPassword()
+    {
+        return password;
+    }
 
-  public void setUsername(String username)
-  {
-    this.username = username;
-  }
+    public String getPassword2nd()
+    {
+        return password2nd;
+    }
 
-  public void setPassword(String password)
-  {
-    this.password = password;
-  }
-  
-  public void setPassword2nd(String password2nd)
-  {
-    this.password2nd = password2nd;
-  }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
 
-  public CreateAccountData(String username, String password, String password2nd)
-  {
-    setUsername(username);
-    setPassword(password);
-    setPassword2nd(password2nd);
-  }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setPassword2nd(String password2nd)
+    {
+        this.password2nd = password2nd;
+    }
+
+    public CreateAccountData(String username, String password, String password2nd) {
+        setUsername(username);
+        setPassword(password);
+        setPassword2nd(password2nd);
+    }
 }
